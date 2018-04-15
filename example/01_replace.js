d17ac1eddeed697a78daa49a7217bd3dff15bed1
hamjungho <pauljungho.ham@gmail.com>
2018-04-15 13:04:42 +0900

    백틱수정

--- example/01_replace.js
+++ example/01_replace.js
@@ -2,4 +2,4 @@ const message = 'hello';
 
 const replaceMessage = message.replace('hello', 'world');
 
-console.log('hello를 world로 바꾸기 : %s', replaceMessage);
+console.log(`hello를 world로 바꾸기 : ${replaceMessage}`);
